2a6495e9130f15910904e06c3a7568053f74e141
Robert <robert.koulakes@gmail.com>
2022-01-11 10:11:24 -0800

    Added basic routes

--- server/controllers/financialController.js
+++ server/controllers/financialController.js
@@ -1,6 +1,15 @@
-//const db = require('../models/dataModel.js');
+const db = require('../models/dataModel.js');
 const financialController = {};
 
+financialController.getFinancials = async (req, res, next) => {
+  const { user_id, created_date } = req.body;
+  const queryString = `
+  SELECT * FROM financials 
+  WHERE user_id = ${user_id} 
+  AND created_date = ${created_date}`;
 
+
+
+};
 
 module.exports = financialController;
