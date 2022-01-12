994906c807593fd34f679b59885d35227d40d7e4
Robert <robert.koulakes@gmail.com>
2022-01-11 22:48:03 -0800

    Updated and finalized middleware for financial routes, cleaned up import statements for actions as well

--- server/controllers/financialController.js
+++ server/controllers/financialController.js
@@ -1,4 +1,3 @@
-const { response } = require('express');
 const db = require('../models/dataModel.js');
 const financialController = {};
 
@@ -6,15 +5,16 @@ financialController.getFinancials = async (req, res, next) => {
   const { email } = req.body;
   const queryString = `
   SELECT * FROM financials 
-  WHERE email = ${email}`;
+  WHERE email = '${email}'`;
   try{
   const availableData = await db.query(queryString);
+  console.log(availableData.rows[0]);
   if(!availableData.rows[0]){
   res.locals.financialsList = [email, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   return next();
   }
   if(availableData.rows[0]){
-    res.locals.financialsList = availableData.rows;
+    res.locals.financialsList = availableData.rows[0];
     return next();
   }
 }
@@ -27,57 +27,42 @@ financialController.getFinancials = async (req, res, next) => {
 };
 
 financialController.postFinancials = async (req, res, next) => {
-  const { email, housing_living, health_medical, auto_transport, education, loans_credit_cards, savings, investment, charity, misc } = req.body;
+  const { email, housing, health, auto, education, loans, savings, investment, charity, misc } = req.body;
   const initialQuery = `
   SELECT * FROM financials 
-  WHERE email = ${email}`;
+  WHERE email = '${email}'`;
   try{
     const availableData = await db.query(initialQuery);
+    console.log(availableData);
     if(!availableData.rows[0]){
-    try{
-    const secondaryQuery = `
-    INSERT INTO financials (email, housing_living, health_medical, auto_transport, education, loans_credit_cards, savings, investment, charity, misc)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
-    RETURNING *`;
-    const values = [email, housing_living, health_medical, auto_transport, education, loans_credit_cards, savings, investment, charity, misc];
-    const insertedFinancials = await db.query(secondaryQuery, values);
-    return next();
+      const secondaryQuery = `
+      INSERT INTO financials (email, housing_living, health_medical, auto_transport, education, loans_credit_cards, savings, investment, charity, misc)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+      RETURNING *`;
+      const values = [email, housing, health, auto, education, loans, savings, investment, charity, misc];
+      const insertedFinancials = await db.query(secondaryQuery, values);
+      return next();
     }
-    catch(err){
-      return next({
-        status: 500,
-        message: `financialController.postFinancials error: ${err}`,
-      })
-    }
-  }
     if(availableData.rows[0]){
-      try{
       let deletionQuery = `
       DELETE FROM financials
-      WHERE email=${email}`;
+      WHERE email='${email}'`;
       const deletedFinancials = await db.query(deletionQuery);
       const tertiaryQuery = `
       INSERT INTO financials (email, housing_living, health_medical, auto_transport, education, loans_credit_cards, savings, investment, charity, misc)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
       RETURNING *`;
-      const newValues = [email, housing_living, health_medical, auto_transport, education, loans_credit_cards, savings, investment, charity, misc];
+      const newValues = [email, housing, health, auto, education, loans, savings, investment, charity, misc];
       const updatedFinancials = await db.query(tertiaryQuery, newValues);
       return next();
     }
+  }
       catch(err){
         return next({
           status: 500,
           message: `financialController.postFinancials error: ${err}`,
         });
       }
-    }
-  }
-    catch(err){
-      return next({
-        status: 500,
-        message: `financialController.postFinancials error: ${err}`,
-      });
-    }
-}
+    };
 
 module.exports = financialController;
--- server/routes/financialRoutes.js
+++ server/routes/financialRoutes.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const financialController = require('../controllers/financialController.js');
 
-const router = express.Router();
+const financeRouter = express.Router();
 
 //GET request to /financial
-router.get('/', financialController.getFinancials, (req, res) => {
+financeRouter.get('/', financialController.getFinancials, (req, res) => {
   res.set('Content-Type', 'application/json').status(200).json({ financialsList: res.locals.financialsList });
 });
 
 //POST request to /financial
-router.post('/', financialController.postFinancials, (req, res) => {
+financeRouter.post('/', financialController.postFinancials, (req, res) => {
   res.status(200).send('Data successfully saved!')
 });
 
 
-module.exports = router;
+module.exports = financeRouter;
